59d140032dc3321494c1a438c13aaa0effe424d4
Gustavo Costa <lksloocky@gmail.com>
2024-08-25 04:43:53 +0900

    Som ambiente e acerto de offbounds

--- Pun K.O ffice/Assets/Scripts/AudioControllerGameplay.cs
+++ Pun K.O ffice/Assets/Scripts/AudioControllerGameplay.cs
@@ -6,23 +6,36 @@ public class AudioControllerGameplay : MonoBehaviour
 {
     public AudioSource audioSourceMusicaDeFundo;
     public AudioSource audioSourceMusicaDeFundo2;
+    public AudioSource audioSourceMusicaDeFundo3;
     
     public AudioClip musicaDeFundo;
     public AudioClip musicaDeFundo2;
+    public AudioClip musicaDeFundo3;
     void Start()
     {
         audioSourceMusicaDeFundo.clip = musicaDeFundo;
         audioSourceMusicaDeFundo.loop = true;
-        audioSourceMusicaDeFundo.Play();     
-    }
+        audioSourceMusicaDeFundo.mute = true;
+        audioSourceMusicaDeFundo.Play();   
 
-    private void Update()
-    {
-        if(ContaKill.Kills == 20)
-        {
         audioSourceMusicaDeFundo2.clip = musicaDeFundo2;
         audioSourceMusicaDeFundo2.loop = true;
-        audioSourceMusicaDeFundo2.Play();     
-        }
+        audioSourceMusicaDeFundo2.mute = true;
+        audioSourceMusicaDeFundo2.Play();  
+
+        audioSourceMusicaDeFundo3.clip = musicaDeFundo3;
+        audioSourceMusicaDeFundo3.loop = true;
+        audioSourceMusicaDeFundo3.Play();   
+  
     }
+
+private void Update()
+{
+        if(ContaKill.Kills == 20)
+        {
+          audioSourceMusicaDeFundo.mute = false;
+          audioSourceMusicaDeFundo2.mute = false;
+          audioSourceMusicaDeFundo3.mute = true;        
+        }
+}
 }
